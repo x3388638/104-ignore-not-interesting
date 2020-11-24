a2799a8e422b08a2236967de3e12f90c3da785a0
YY <yuying.chang@verizonmedia.com>
2020-11-25 00:19:57 +0800

    Add userscript meta

---
+++ meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         104-ignore-not-interesting
+// @namespace    https://2yc.tw
+// @version      1.0.0
+// @description  Ignore jobs you are not interested in on 104
+// @author       YY
+// @match        https://www.104.com.tw/jobs/search/*
+// @match        https://www.104.com.tw/job/*
+// @license      MIT
+// @homepage     https://github.com/x3388638/104-ignore-not-interesting
+// @updateURL    https://openuserjs.org/meta/x3388638/104-ignore-not-interesting.meta.js
+// @grant        none
+// ==/UserScript==
